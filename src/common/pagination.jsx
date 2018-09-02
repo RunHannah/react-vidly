c9975058df89652783c66690abdef95c049632fa
Hannah Lee <hanlavlee@gmail.com>
2018-09-01 20:40:07 -0700

    Pagination is active when clicked.

--- src/common/pagination.jsx
+++ src/common/pagination.jsx
@@ -4,7 +4,8 @@ import _ from "lodash";
 // Think about inputs and outputs
 
 const Pagination = props => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
 
   const pagesCount = Math.ceil(itemsCount / pageSize);
 
@@ -15,8 +16,13 @@ const Pagination = props => {
     <nav>
       <ul className="pagination">
         {pages.map(page => (
-          <li key={page} className="page-item">
-            <a className="page-link">{page}</a>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
           </li>
         ))}
       </ul>
