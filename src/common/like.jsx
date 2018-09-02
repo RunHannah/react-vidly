d6d7a32240faea1bb9e69b9307b8e090dafc826f
Hannah Lee <hanlavlee@gmail.com>
2018-09-01 19:58:24 -0700

    Add dynamic like component as a stateless functional component.

---
+++ src/common/like.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+// Input: liked: boolean
+// Output: onClick
+
+// This component recieves data from props, so
+// Simplified to be a Stateless Functional Component below
+// remove references to "this"
+
+// class Like extends Component {
+//   render() {
+//     let classes = "fa fa-heart";
+//     if (!this.props.liked) classes += "-o";
+//     return (
+//       <i
+//         onClick={this.props.onClick}
+//         style={{ cursor: "pointer" }}
+//         className={classes}
+//         aria-hidden="true"
+//       />
+//     );
+//   }
+// }
+
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
