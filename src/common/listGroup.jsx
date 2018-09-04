fd59ca9bb39a20212975f64a1c989c251bcb12b7
Hannah Lee <leexhannah@gmail.com>
2018-09-03 18:32:41 -0700

    Refactor selecting item genre action.

--- src/common/listGroup.jsx
+++ src/common/listGroup.jsx
@@ -4,17 +4,34 @@ import React, { Component } from "react";
 // so ListGroup can by reused by any list that may not have those property names
 // look at movies under ListGroup
 const ListGroup = props => {
-  const { items, textProperty, valueProperty } = props;
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    selectedItem,
+    onItemSelect
+  } = props;
 
   return (
     <ul className="list-group">
       {items.map(item => (
-        <li key={item[valueProperty]} className="list-group-item">
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
           {item[textProperty]}
         </li>
       ))}
     </ul>
   );
+};
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id"
 };
 
 export default ListGroup;
