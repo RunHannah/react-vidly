f066886706e916bf281af0363f1cf3365aac06ad
Hannah Lee <leexhannah@gmail.com>
2018-09-03 23:28:34 -0700

    Refactor filtering, working on sorting.

---
+++ src/common/tableBody.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import _ from "lodash";
+
+class TableBody extends Component {
+  render() {
+    const { data, columns } = this.props;
+    return <tbody />;
+  }
+}
+
+export default TableBody;
