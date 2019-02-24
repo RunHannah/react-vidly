4ca32176e0680d27a990043a662a1bf7c0ed49f4
Hannah Lee <leexhannah@gmail.com>
2019-02-23 17:51:13 -0800

    Add a basic validation method for form input.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,24 +3,56 @@ import Input from '../common/input';
 
 export class LoginForm extends Component {
   state = {
-    account: { username: '', password: '' }
+    account: { username: '', password: '' },
+    errors: {}
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+
+    if (account.username.trim() === '')
+      errors.username = 'Username is required';
+    if (account.password.trim() === '')
+      errors.password = 'Password is required';
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = e => {
     e.preventDefault();
 
+    const errors = this.validate();
+
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
     // Call the server
     console.log('Submitted');
   };
 
-  handleChange = e => {
+  validateProperty = ({ name, value }) => {
+    if (name === 'username') {
+      if (value.trim() === '') return 'Username is required';
+    }
+    if (name === 'password') {
+      if (value.trim() === '') return 'Password is required';
+    }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
-    account[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ account });
+    account[input.name] = input.value;
+
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -31,12 +63,14 @@ export class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
