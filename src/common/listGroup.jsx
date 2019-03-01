313875590429133594b9480d09cf9344a90bd14a
Hannah Lee <leexhannah@gmail.com>
2019-03-01 15:52:09 -0800

    Connect and render movies from api and refactor methods populating movie and genres.

--- src/common/listGroup.jsx
+++ src/common/listGroup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 // filtering items using valueProperty and itemProperty rather than property names
 // so ListGroup can by reused by any list that may not have those property names
@@ -17,7 +17,7 @@ const ListGroup = ({
           onClick={() => onItemSelect(item)}
           key={item[valueProperty]}
           className={
-            item === selectedItem ? "list-group-item active" : "list-group-item"
+            item === selectedItem ? 'list-group-item active' : 'list-group-item'
           }
         >
           {item[textProperty]}
@@ -28,8 +28,8 @@ const ListGroup = ({
 };
 
 ListGroup.defaultProps = {
-  textProperty: "name",
-  valueProperty: "_id"
+  textProperty: 'name',
+  valueProperty: '_id'
 };
 
 export default ListGroup;
