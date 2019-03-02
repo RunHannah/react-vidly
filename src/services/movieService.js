a0e6a1b474dd8b632ba7a11fd0d2e34473d40864
Hannah Lee <leexhannah@gmail.com>
2019-03-01 16:10:35 -0800

    Update to save movie.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,16 +3,30 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoint = apiUrl + '/movies';
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + '/' + movieId);
+  return http.get(movieUrl(movieId));
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    // remove id from movie object so there is only one id (from url) sent back to api
+    // make a copy of movie and send as body
+
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie), body);
+  }
+  return http.post(apiEndpoint, movie);
+}
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + '/' + movieId);
+  return http.delete(movieUrl(movieId));
 }
